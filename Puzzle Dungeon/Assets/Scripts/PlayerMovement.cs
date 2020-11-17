6792e774cf67247accaf32fd921b7bcaaf98dadc
jsturgi <jsturgi@iu.edu>
2020-11-17 13:59:25 -0500

    player movement, inputs mapped

---
+++ Puzzle Dungeon/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementLoop();
+    }
+
+    private void MovementLoop()
+    {
+        if (Input.GetButtonDown("Forward"))
+        {
+            this.transform.position += new Vector3(0,1,0);
+        }
+
+        if (Input.GetButtonDown("Backward"))
+        {
+            this.transform.position -= new Vector3(0,1,0);
+        }
+
+        if (Input.GetButtonDown("Right"))
+        {
+            this.transform.position += Vector3.right;
+        }
+
+        if (Input.GetButtonDown("Left"))
+        {
+            this.transform.position += Vector3.left;
+        }
+
+    }
+
+
+}
