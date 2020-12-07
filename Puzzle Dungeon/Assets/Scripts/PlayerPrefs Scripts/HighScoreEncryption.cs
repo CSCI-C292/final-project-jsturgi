63191c9bba5dcd9cb676bfce5434fc58678ed235
jsturgi <jsturgi@iu.edu>
2020-12-07 13:53:28 -0500

    pain and suffering

---
+++ Puzzle Dungeon/Assets/Scripts/PlayerPrefs Scripts/HighScoreEncryption.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public class HighScoreEncryption : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetPlayerPrefs(string name, string time, string rank)
+    {
+        ZPlayerPrefs.SetString(name, name);
+        ZPlayerPrefs.SetString(time, time);
+        ZPlayerPrefs.SetString(rank, rank);
+    }
+}
